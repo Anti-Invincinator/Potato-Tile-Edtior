6accbb4c3fc2912fa8257bd2849e8421f45983da
Anti_Invincinator <kpdravid@gmail.com>
2021-04-18 02:57:56 +0530

    View

--- Potato_TileEditor/GameState.cpp
+++ Potato_TileEditor/GameState.cpp
@@ -137,22 +137,28 @@ void GameState::updateView(const float& dt)
 		std::floor(this->player->getPosition().y + (static_cast<float>(this->mousePosWindow.y) - static_cast<float>(this->stateData->gfxSettings->resolution.height / 2)) / 10.f)
 	);
 
-	if (this->view.getCenter().x - this->view.getSize().x / 2.f < 0.f)
+	if (this->tileMap->getMaxSizeF().x >= this->view.getSize().x)
 	{
-		this->view.setCenter(0.f + this->view.getSize().x / 2.f, this->view.getCenter().y);
-	}
-	else if (this->view.getCenter().x + this->view.getSize().x / 2.f > this->tileMap->getMaxSizeF().x)
-	{
-		this->view.setCenter(this->tileMap->getMaxSizeF().x - this->view.getSize().x / 2.f, this->view.getCenter().y);
+		if (this->view.getCenter().x - this->view.getSize().x / 2.f < 0.f)
+		{
+			this->view.setCenter(0.f + this->view.getSize().x / 2.f, this->view.getCenter().y);
+		}
+		else if (this->view.getCenter().x + this->view.getSize().x / 2.f > this->tileMap->getMaxSizeF().x)
+		{
+			this->view.setCenter(this->tileMap->getMaxSizeF().x - this->view.getSize().x / 2.f, this->view.getCenter().y);
+		}
 	}
 
-	if (this->view.getCenter().y - this->view.getSize().y, 0.f + this->view.getSize().y / 2.f)
+	if (this->tileMap->getMaxSizeF().y >= this->view.getSize().y)
 	{
-		this->view.setCenter(this->view.getCenter().x, 0.f + this->view.getSize().y / 2.f);
-	}
-	else if (this->view.getCenter().y + this->view.getSize().y, this->view.getSize().y / 2.f > this->tileMap->getMaxSizeF().y)
-	{
-		this->view.setCenter(this->view.getCenter().x, this->tileMap->getMaxSizeF().y - this->view.getSize().y / 2.f);
+		if (this->view.getCenter().y - this->view.getSize().y / 2.f < 0.f)
+		{
+			this->view.setCenter(this->view.getCenter().x, 0.f + this->view.getSize().y / 2.f);
+		}
+		else if (this->view.getCenter().y + this->view.getSize().y / 2.f > this->tileMap->getMaxSizeF().y)
+		{
+			this->view.setCenter(this->view.getCenter().x, this->tileMap->getMaxSizeF().y - this->view.getSize().y / 2.f);
+		}
 	}
 
 	this->viewGridPosition.x = static_cast<int>(this->view.getCenter().x) / static_cast<int>(this->stateData->gridSize);
